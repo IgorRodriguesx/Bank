b4fda9756c18f5976c734703bff8f7d32b27cc0f
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2023-10-05 11:11:01 -0300

    Update CreateConta.java

--- CreateConta.java
+++ CreateConta.java
@@ -1,20 +1,23 @@
 
 public class CreateConta {
 	public static void main(String[] args) {
+		
 		Conta primeiraConta = new Conta();
 		Conta segundaConta = new Conta();
+		
 		primeiraConta.saldo = 1000;
 		segundaConta.saldo = 2000;
+		
 		System.out.println("Valor atual da conta 1 seria de:" + primeiraConta.saldo + " e da conta 2 seria de: " + segundaConta.saldo);
 		
 		primeiraConta.saldo += 100;
 		segundaConta.saldo += 200;
+		
 		System.out.println("Saldo atual da conta 1:"+ primeiraConta.saldo +" e Saldo atual da conta 2: "+ segundaConta.saldo );
-		
-		
 		
 		primeiraConta.agencia = 100;
 		segundaConta.agencia = 200;
+		
 		System.out.println("Agência da conta 1 e da conta 2 respectivamente: "+ primeiraConta.agencia +" e "+ segundaConta.agencia );
 		
 		if(primeiraConta == segundaConta) {
