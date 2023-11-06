3ab4d6590bac42cc42fd387691dbe2daf5eeda2d
Igor Rodrigues de Souza <111098178+IgorRodriguesx@users.noreply.github.com>
2023-11-06 19:51:03 -0300

    Update TestValores.java

--- TestValores.java
+++ TestValores.java
@@ -1,14 +1,7 @@
-
 public class TestValores {
 	public static void main(String[] args) {
-		Conta conta = new Conta();
-		
-		//Conta incosistente
-		conta.setAgencia(-50);
-		conta.setNumero(-330);
-		
+		Conta conta = new Conta(1001, 9090);
 		System.out.println(conta.getAgencia());
-		
-		
+		Conta conta2 = new Conta(2002, 8080);
 	}
 }
